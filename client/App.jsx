4f5854425b32d3bfcaa4c25edd79b775aaad7251
pardocch <my900002@gmail.com>
2020-08-12 21:55:43 +0800

    init

---
+++ client/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const App = () => {
+
+  return (
+    <div className="container">
+      <h1>empty project</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ client/store.js
@@ -0,0 +1,7 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import reducer from './reducers/reducer';
+import thunk from 'redux-thunk';
+
+const store = createStore(reducer, applyMiddleware(thunk));
+
+export default store;
